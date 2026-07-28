8183480bb5205c902358129f3c8305506f983917
PRASHIK16 <prashikdongre3937@gmail.com>
2026-07-29 00:30:22 +0530

    afterSignInUrl deprecation

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -20,7 +20,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+  signInFallbackRedirectUrl="/dashboard"
+  signUpFallbackRedirectUrl="/dashboard"
+    >
       <html lang="en" suppressHydrationWarning>
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <QueryProvider>
